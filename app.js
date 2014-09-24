b1ad6581dd90a7e5d12d995378d43c9dc0cab96c
Joseph McCarron <joe@zendesk.com>
2014-09-24 05:27:12 -0700

    basic pagination and cleanup

--- app.js
+++ app.js
@@ -15,9 +15,12 @@
 
       'click button.addFilter':'onAddFilterClicked',
       'click button.search':'onSearchClicked',
+      'click a.prev_page':'onPrevClicked',
+      'click a.next_page':'onNextClicked',
 
       // request events
-      'search.done':'onSearchComplete'
+      'search.done':'onSearchComplete',
+      'getUrl.done':'onSearchComplete'
     },
     requests: {
       // searchIncremental: function(query, sort_by, sort_order, page) {
@@ -136,95 +139,119 @@
     },
     onSearchClicked: function(e) {
       if (e) {e.preventDefault();}
+      this.$('div.results').html('');
       var string = this.$('input.string').val();
-      if (string.length >= 0) {
-        // this is where we add filters before searching on /incremental
-        var type = this.$('select.type').val();
-        var filter_string = '';
-        switch (type) {
-          case "ticket":
+      // this is where we add filters before searching on /incremental
+      var type = this.$('select.type').val();
+      var filter_string = '';
+      switch (type) {
+        case "ticket":
 
-            var status_operator = '';
-            if(this.$('select.status_operator').val() == 'greater') {
-              status_operator = '>';
-            } else if (this.$('select.status_operator').val() == 'less') {
-              status_operator = '<';
-            } else if (this.$('select.status_operator').val() == ':') {
-              status_operator = ':';
-            }
-            var priority_operator = '';
-            if(this.$('select.priority_operator').val() == 'greater') {
-              priority_operator = '>';
-            } else if (this.$('select.priority_operator').val() == 'less') {
-              priority_operator = '<';
-            } else if (this.$('select.priority_operator').val() == ':') {
-              priority_operator = ':';
-            }
-            var date_operator = '';
-            if(this.$('select.date_operator').val() == 'greater') {
-              date_operator = '>';
-            } else if (this.$('select.date_operator').val() == 'less') {
-              date_operator = '<';
-            } else if (this.$('select.date_operator').val() == ':') {
-              date_operator = ':';
-            }
-            var ticket_filters = {
-              "status": status_operator + this.$('select.status_value').val(),
-              "ticket_type": this.$('select.ticket_type').val(),
-              "priority": priority_operator + this.$('select.priority_value').val(),
-              "date": this.$('select.date_type').val() + date_operator + this.$('input.date_value').val(),
-              "group": this.$('input.group').val(),
-              "assignee": this.$('input.assignee').val(),
-              "submitter": this.$('input.submitter').val(),
-              "organization": this.$('input.organization').val(),
-              "requester": this.$('input.requester').val(),
-              "commenter": this.$('input.commenter').val(),
-              "cc": this.$('input.cc').val(),
-              "subject": this.$('input.subject').val(),
-              "description": this.$('input.description').val(),
-              "tags": this.$('input.tags').val(), //.split(/\W/)
-              "via": this.$('select.via').val()
-            };
+          var status_operator = '';
+          if(this.$('select.status_operator').val() == 'greater') {
+            status_operator = '>';
+          } else if (this.$('select.status_operator').val() == 'less') {
+            status_operator = '<';
+          } else if (this.$('select.status_operator').val() == ':') {
+            status_operator = ':';
+          }
+          var priority_operator = '';
+          if(this.$('select.priority_operator').val() == 'greater') {
+            priority_operator = '>';
+          } else if (this.$('select.priority_operator').val() == 'less') {
+            priority_operator = '<';
+          } else if (this.$('select.priority_operator').val() == ':') {
+            priority_operator = ':';
+          }
+          var date_operator = '';
+          if(this.$('select.date_operator').val() == 'greater') {
+            date_operator = '>';
+          } else if (this.$('select.date_operator').val() == 'less') {
+            date_operator = '<';
+          } else if (this.$('select.date_operator').val() == ':') {
+            date_operator = ':';
+          }
+          var ticket_filters = {
+            "status": status_operator + this.$('select.status_value').val(),
+            "ticket_type": this.$('select.ticket_type').val(),
+            "priority": priority_operator + this.$('select.priority_value').val(),
+            "date": this.$('select.date_type').val() + date_operator + this.$('input.date_value').val(),
+            "group": this.$('input.group').val(),
+            "assignee": this.$('input.assignee').val(),
+            "submitter": this.$('input.submitter').val(),
+            "organization": this.$('input.organization').val(),
+            "requester": this.$('input.requester').val(),
+            "commenter": this.$('input.commenter').val(),
+            "cc": this.$('input.cc').val(),
+            "subject": this.$('input.subject').val(),
+            "description": this.$('input.description').val(),
+            "tags": this.$('input.tags').val(), //.split(/\W/)
+            "via": this.$('select.via').val()
+          };
 
-            // render a template to build the filters string
-            filter_string = this.renderTemplate('ticket_filter_string', {
-              filters: ticket_filters
-            });
-          break;
-        } // end case
+          // render a template to build the filters string
+          filter_string = this.renderTemplate('ticket_filter_string', {
+            filters: ticket_filters
+          });
+        break;
+      } // end case
 
-        //no matter the type...
-        this.results = [];
-        var query = string + filter_string + ' type:' + type,
-          sort_by = this.$('select.sort_by').val(),
-          sort_order = this.$('select.sort_order').val(),
-          page = '1';
+      //no matter the type...
+      this.results = [];
+      var query = string + filter_string + ' type:' + type,
+        sort_by = this.$('select.sort_by').val(),
+        sort_order = this.$('select.sort_order').val(),
+        page = '1';
+      if(query.length < 2) {
+        services.notify("A search query must have at least two characters.", "error");
+      } else {
         var encodedQuery = encodeURIComponent(query);
+        // store the query globally
+        this.encodedQuery = encodedQuery;
         
         this.$("span.no_results").hide();
         this.$("span.loading").show();
         // make the request
         this.ajax('search', encodedQuery, sort_by, sort_order, page);
-
-        
-      } else {
-        // pop an alert that a search must have at least two characters
-        services.notify("A search query must have at least two characters.", "error");
       }
+    },
+    onPrevClicked: function(e) {
+      e.preventDefault();
+      this.results = [];
+      this.ajax('getUrl', this.prev_page);
+      this.$('div.results').html('');
+      this.$("span.loading").show();
+    },
+    onNextClicked: function(e) {
+      e.preventDefault();
+      this.results = [];
+      this.ajax('getUrl', this.next_page);
+      this.$('div.results').html('');
+      this.$("span.loading").show();
     },
     onSearchComplete: function(response) {
       var allPages = this.$('.all_pages').prop('checked');
       this.results = this.results.concat(response.results);
       if(allPages && response.next_page) {
         // get the next page by URL
-        this.ajax('getUrl', response.next_page).done(function(response) {
-          this.onSearchComplete(response);
-        });
+        this.ajax('getUrl', response.next_page);
         return;
       } else {
-        
+        // TODO: capture the page URLs and add buttons # numbering
+        if(response.next_page) {
+          this.next_page = response.next_page;
+        }
+        if(response.previous_page) {
+          this.prev_page = response.previous_page;
+        }
+        this.numberOfResults = response.count;
       }
       var results = this.results;
+      if(results.length === 0) {
+        this.$("span.loading").hide();
+        this.$('span.no_results').show();
+        return;
+      }
       this.encoded = [];
       _.each(results, function(result, n) {
       // massage the data...
@@ -235,7 +262,7 @@
         result.updated_at = new Date(result.updated_at);
         result.updated_at = result.updated_at.toLocaleString();
         
-        // encode results in range
+        // encode results
         this.encoded[n] = {
           type: encodeURIComponent(result.type),
           id: encodeURIComponent(result.id),
@@ -263,11 +290,12 @@
           result.status_label = '<span class="ticket_status_label closed">closed</span>';
         }
       }.bind(this));
-
+      
       // display results
       var results_html = this.renderTemplate('results', {
         results: results,
-        encoded_results: this.encoded
+        encoded_results: this.encoded,
+        count: this.numberOfResults
       });
       this.$("span.loading").hide();
       this.$('div.results').html(results_html);
